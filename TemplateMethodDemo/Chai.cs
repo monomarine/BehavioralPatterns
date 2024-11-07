2ec30a83c933accbbe7c0c59f96a47f83e54dfa1
ilya <93557674+Disline1337@users.noreply.github.com>
2024-11-07 13:05:40 +0500

    added check for whitespace && new class

---
+++ TemplateMethodDemo/Chai.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodDemo
+{
+    internal class Chai : Dish
+    {
+        public override void Cook()
+        {
+            Console.WriteLine("наливаем кипяток");
+        }
+
+        public override void PrepareIngridients()
+        {
+            Console.WriteLine("завариваем чай");
+        }
+
+        public override void Serving()
+        {
+            Console.WriteLine("сыпим сахар");
+        }
+    }
+}
